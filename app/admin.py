935a264e9ee7e2d0709340673bc5a5d53f78f4f2
Foram <foramdadhaniya1111@gmail.com>
2023-10-10 10:41:45 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import (
+    Customer,Product,Cart,Orderplaced
+)
+# Register your models here.
+@admin.register(Customer)
+class Customermodeladmin(admin.ModelAdmin):
+    list_display = ['id','user','name','locality','city','zipcode','state']
+
+@admin.register(Product)
+class Productmodeladmin(admin.ModelAdmin):
+    list_display = ['id','title','selling_price','discounted_price','description','brand','category','product_image']
+    
+
+@admin.register(Cart)
+class Cartmodeladmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
+
+
+
+@admin.register(Orderplaced)
+class Orderplacedmodeladmin(admin.ModelAdmin):
+    list_display = ['id','user','customer','product','quantity','ordered_date','status']
